dac3bd5cd8ec28516dbd5dbde03f5c9243dca1b8
MarVikt <v.m.podporogie@gmail.com>
2022-04-13 23:06:34 +0300

    switching tasks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,64 @@
-console.log('fffr');
+const formTask = document.querySelector('.todo-control');
+// const buttonPlus = document.getElementById('add');
+// const buttonsRemove = document.querySelectorAll('.todo-remove');
+// const buttonsCompleted = document.querySelectorAll('.todo-complete');
+const todoList = document.querySelector('.todo-list');
+const completedList = document.querySelector('.todo-completed');
+
+// const taskList = [];
+const taskList = [
+  {
+    text: "Сварить кофе",
+    completed: false
+  },
+  {
+    text: "Помыть посуду",
+    completed: true
+  }
+];
+
+formTask.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const textTask = document.querySelector('.header-input');
+
+  if (textTask.value.length > 0) {
+    const newTask = {
+      text: textTask.value,
+      completed: false
+    };
+    taskList.push(newTask);
+    textTask.value = '';
+    updateList();
+  }
+});
+
+function updateList() {
+  todoList.innerHTML = '';
+  completed.innerHTML = '';
+  taskList.forEach(function(item) {
+    const listItem = document.createElement('li');
+  
+    listItem.classList.add('todo-item');
+    listItem.innerHTML = '<span class="text-todo">' + item.text +'</span>' +
+      '<div class="todo-buttons">' + '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' + '</div>';
+    
+    if (item.completed) {
+      completedList.append(listItem);
+    } else {
+      todoList.append(listItem);
+    }
+
+    listItem.querySelector('.todo-complete').addEventListener('click', function () {
+      item.completed = !item.completed;
+      updateList();
+    });   
+
+  });
+}
+
+updateList();
+
+console.log(taskList);
+
+
